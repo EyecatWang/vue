d0e1b5d3e54aa76ec5185ea6a5b406cfcee2faa8
ye <1375032804@qq.com>
2019-01-31 11:57:02 +0800

    vue源码解读第一步 修改ignore

--- src/core/instance/init.js
+++ src/core/instance/init.js
@@ -28,6 +28,7 @@ export function initMixin (Vue: Class<Component>) {
 
     // a flag to avoid this being observed
     vm._isVue = true
+    // 第一步校验参数
     // merge options
     if (options && options._isComponent) {
       // optimize internal component instantiation
